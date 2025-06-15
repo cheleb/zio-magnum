a4f2eb871d3acd7ed011de558fb5db2378a02215
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2025-06-15 15:41:54 +0200

    Immuntable Repository Support

--- modules/zio-magnum/src/test/scala/com/augustnagro/magnum/ziomagnum/QuerySpec.scala
+++ modules/zio-magnum/src/test/scala/com/augustnagro/magnum/ziomagnum/QuerySpec.scala
@@ -8,24 +8,12 @@ import javax.sql.DataSource
 import zio.logging.backend.SLF4J
 import zio.logging.LogFormat
 
-@SqlName("users")
-@Table(PostgresDbType, SqlNameMapper.CamelToSnakeCase)
-case class User(id: Int, name: String) derives DbCodec
-
-val userRepo = ImmutableRepo[User, Int]
-
-object MeshRepositorySpec
-    extends ZIOSpecDefault
-    with RepositorySpec("sql/users.sql") {
+object QuerySpec extends ZIOSpecDefault with RepositorySpec("sql/users.sql") {
 
   val slf4jLogger: ULayer[Unit] = Runtime.removeDefaultLoggers >>> SLF4J.slf4j
 
   override def spec: ZSpec[TestEnvironment & Scope, Any] =
     suite("ZIO Magnum")(
-      test("ImmutableRepo ") {
-        userRepo.zcount
-          .map(count => assert(count)(equalTo(5)))
-      },
       test("Queying a table") {
         sql"SELECT * FROM users"
           .zQuery[User]
@@ -36,7 +24,7 @@ object MeshRepositorySpec
         val program = for {
           _ <- ZIO.logDebug("Starting stream")
           zs = sql"SELECT * FROM users".zStream[User]()
-          _ <- zs.runForeach(user => ZIO.debug(s"User from stream: $user"))
+          _ <- zs.runForeach(user => ZIO.logDebug(s"User from stream: $user"))
           count <- zs.runCount
 
         } yield count
