e086ee21cc2b1cae5723da42cd21b11333ccfc40
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2026-04-29 08:57:43 +0200

    fix(test): update PostgreSQLContainer import and type for testcontainers v0.44.1
    Update the import path and remove the `Nothing` type parameter for `PostgreSQLContainer` to maintain compatibility with the updated testcontainers version.

--- modules/zio-magnum/src/test/scala/com/augustnagro/magnum/ziomagnum/RepositorySpec.scala
+++ modules/zio-magnum/src/test/scala/com/augustnagro/magnum/ziomagnum/RepositorySpec.scala
@@ -5,7 +5,7 @@ import zio.*
 import javax.sql.DataSource
 
 import org.postgresql.ds.PGSimpleDataSource
-import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.postgresql.PostgreSQLContainer
 
 import zio.logging.backend.SLF4J
 import scala.language.implicitConversions
@@ -16,15 +16,15 @@ trait RepositorySpec(init: String) {
 
   val slf4jLogger: ULayer[Unit] = Runtime.removeDefaultLoggers >>> SLF4J.slf4j
 
-  private def postgres(): PostgreSQLContainer[Nothing] =
-    val container: PostgreSQLContainer[Nothing] =
+  private def postgres(): PostgreSQLContainer =
+    val container: PostgreSQLContainer =
       PostgreSQLContainer("postgres")
         .withInitScript(init)
     container.start()
     container
 
   private def createDataSource(
-      container: PostgreSQLContainer[Nothing]
+      container: PostgreSQLContainer
   ): DataSource =
     val dataSource = new PGSimpleDataSource()
     dataSource.setUrl(container.getJdbcUrl)
